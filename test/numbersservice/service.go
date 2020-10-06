05bcb14b4b7861b8c93fc1bb58aaecda23e7b180
Tiny Paws <bastienmillet073@gmail.com>
2020-10-06 13:48:37 +0200

    Adding tests to the Sub method of NumbersService

--- test/numbersservice/service.go
+++ test/numbersservice/service.go
@@ -13,4 +13,10 @@ func TestNumbersService(t *testing.T) {
 	if err != nil || got != expected {
 		t.Error("svc.Add(5, 5) =", got, "; Want", expected)
 	}
+
+	expected = 5
+	got, err = svc.Sub(15, 10)
+	if err != nil || got != expected {
+		t.Error("svc.Sub(15, 10) =", got, "; Want", expected)
+	}
 }
